131c1c6fc5f195d42acdf0c56f8add564ecfa817
nadahelly <nadahelly@naver.com>
2023-08-30 18:12:55 +0900

    slide

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,13 +1,34 @@
+// var swiper = new Swiper(".slide-intro", {
+//   slidesPerView: 1,
+//   spaceBetween: 3,
+//   loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+// main-slide
 var swiper = new Swiper(".slide-intro", {
-  slidesPerView: 1,
-  spaceBetween: 3,
+  spaceBetween: 0,
+  centeredSlides: true,
   loop: true,
+  loopAdditionalSlides: 1,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+// footer-bnr slide
